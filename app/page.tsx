aa8e2d6910f28c20c4efaa6a9b0c6307433c78ce
Asonjay <49351464+Asonjay@users.noreply.github.com>
2025-01-11 15:53:26 -0600

    Added News

--- app/page.tsx
+++ app/page.tsx
@@ -156,7 +156,7 @@ export default async function Page() {
           <Papers24 />
         </span>
         <span className="mt-3 mb-2 uppercase tracking-[.2em] text-accent">
-          Publications
+          Selected Publications
         </span>
       </div>
       <PublicationList selectedOnly={true} />
